a369aa81f7056a11c8160a08a2b37f896274eca9
lg <lg4869@outlook.com>
2022-11-22 11:51:31 +0000

    simplify

--- plugins/plugins.go
+++ plugins/plugins.go
@@ -15,24 +15,9 @@ type (
 )
 
 var (
-	Run = execution.Run
-	Out = execution.Output
-
-	Par = control.Par
-	Seq = control.Seq
-
-	Echo = builtin.Echo
-	Fn   = builtin.Fn
-	FnOk = builtin.FnOk
 	SH   = builtin.Shell
 	SSH  = builtin.SSH
-
-	Ignore = control.Ignore
-	Lmd    = control.Lambda
-	Tee    = control.Tee
-	Term   = control.Term
-	Try    = control.Try
-	TryI   = control.TryI
+	Term = control.Term
 )
 
 type UserHost struct{ User, Host string }
@@ -45,21 +30,21 @@ func At(u, h string) UserHost {
 }
 
 func PC(prog string, args ...string) P {
-	return Psh(proc.Proc{Prog: prog, Args: args})
+	return Psh(Proc{Prog: prog, Args: args})
 }
 
 func RPC(host string, prog string, args ...string) P {
-	return SSH(proc.Proc{Prog: prog, Args: args, Host: host})
+	return SSH(Proc{Prog: prog, Args: args, Host: host})
 }
 
 func Urpc(uh UserHost, prog string, args ...string) P {
-	return SSH(proc.Proc{Prog: prog, Args: args, Host: uh.Host, User: uh.User})
+	return SSH(Proc{Prog: prog, Args: args, Host: uh.Host, User: uh.User})
 }
 
 func Trpc(ps1, host string, prog string, args ...string) P {
 	return Term(ps1, RPC(host, prog, args...))
 }
 
-func Psh(p proc.Proc) P { return SH(p.CmdCtx(context.TODO())) }
+func Psh(p Proc) P { return SH(p.CmdCtx(context.TODO())) }
 
 func Ps(p ...P) []P { return p }
