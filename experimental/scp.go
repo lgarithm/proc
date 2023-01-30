f54850329c268115911d3bdc35f88fb43b5a4c32
lg <lg4869@outlook.com>
2023-01-30 14:03:37 +0000

    scp: StrictHostKeyChecking=no

--- experimental/scp.go
+++ experimental/scp.go
@@ -3,7 +3,11 @@ package experimental
 func Scp(uh At, local, remote string) P {
 	p0 := Proc{
 		Prog: `scp`,
-		Args: []string{local, uh.User + `@` + uh.Host + ":" + remote},
+		Args: []string{
+			`-o`, `StrictHostKeyChecking=no`,
+			local,
+			uh.User + `@` + uh.Host + ":" + remote,
+		},
 	}
 	return seq(
 		psh(p0),
